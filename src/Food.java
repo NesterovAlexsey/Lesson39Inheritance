0ceb028dfa118524e351f7782c9349e88b26ab42
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-14 15:37:24 +0100

    Add parameters, setters, getters to class Food

---
+++ src/Food.java
@@ -0,0 +1,35 @@
+//Задача 1
+//    Создать класс Food (еда). В классе должны быть:
+//
+//    поле "название"
+//    поле "калорийность"
+//    конструктор, геттеры, сеттеры
+//    метод "употребить еду" с каким-нибудь выводом
+
+public class Food {
+
+  private String name;
+  private int calories;
+
+  public Food(String name, int calories) {
+    this.name = name;
+    this.calories = calories;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getCalories() {
+    return calories;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setCalories(int calories) {
+    this.calories = calories;
+  }
+
+}
